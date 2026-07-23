dcd18358f20ff78b19c39e8080e17c7cad72e984
tangshanhans-code <tangshanhans@gmail.com>
2026-07-23 16:50:57 +0100

    Update CityDetail.jsx

--- src/pages/CityDetail.jsx
+++ src/pages/CityDetail.jsx
@@ -188,7 +188,7 @@ export default function CityDetail({ cityName, goBack }) {
       {/* 图片流区域 */}
       <div className="gallery-section">
         <div className="gallery-container">
-          <h2 className="gallery-title">测试</h2>
+          <h2 className="gallery-title">记录瞬间</h2>
           <div className="gallery-grid">
             {currentCity.gallery.length > 0 ? (
               currentCity.gallery.map((image, index) => {
